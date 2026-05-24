fa09d25dd5fc2d0de3f74a7525fc3d1e64b9e7d4
stevegv007 <stevegv007@gmail.com>
2026-05-24 21:22:40 +0530

    server side full

--- socket server try.py
+++ socket server try.py
@@ -2,4 +2,32 @@ import socket
 import variable
 
 print('ill try sockets in this file')
-sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+HOST = '127.0.0.1'
+PORT = 5555
+
+sock.bind((HOST, PORT))
+
+sock.listen()
+
+print("server is listening...")
+
+conn, addr = sock.accept()
+
+print(f"Connected to {addr}")
+
+while True:
+    message = conn.recv(1024).decode()
+
+    if not message:
+        break
+
+    print(f"Client: {message}")
+
+    reply = input("You: ")
+
+    conn.send(reply.encode())
+
+conn.close()
+sock.close()
